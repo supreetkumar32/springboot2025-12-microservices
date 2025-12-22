811388193fabaa5183e107178679aba5b9437210
Supreet <supreetkumar32@gmail.com>
2025-12-22 16:10:40 +0530

    inventory service skeleton got created

---
+++ inventory-service/inventory-service/src/main/java/com/twelve/microservice/inventory_service/controller/ProductController.java
@@ -0,0 +1,32 @@
+package com.twelve.microservice.inventory_service.controller;
+
+import com.twelve.microservice.inventory_service.dto.ProductDto;
+import com.twelve.microservice.inventory_service.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<ProductDto>> getAllInventory() {
+        List<ProductDto> inventories = productService.getAllInventory();
+        return ResponseEntity.ok(inventories);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDto> getInventoryById(@PathVariable Long id) {
+        ProductDto inventory = productService.getProductById(id);
+        return ResponseEntity.ok(inventory);
+    }
+
+}
